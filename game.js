dcb7dfb397aebcd4554dd0f3dcf7bac79d2ad328
Darryl Johnson <dandycodes@gmail.com>
2023-01-30 20:02:14 -0500

    ready for fighter functionality

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-window.alert("Javascript is running my guy!")
+// in order for this game to work we will need functions for
+// game start, game end, fighting points(randomized), enemy abilities, and power ups
+
+
+let fighterName = window.prompt("What is the name of your fighter?")
+window.alert(fighterName)
+console.log(fighterName)
+console.log("This log is for strings, great for leaving messages")
+console.log(10 + 10)
+console.log("Our fighters Name is " + fighterName)
+
+
+
+// function below will start the game/battle
+function fight() {
+    window.alert('Let The Game Begin!!!')
+}
+
+
+//callback function
+// fight(); dont need this line to run yet
