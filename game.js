491e74007813114789350dfec1c85ffc7818babc
Darryl Johnson <dandycodes@gmail.com>
2023-01-30 21:53:45 -0500

    added debugger to initilializing loop

--- game.js
+++ game.js
@@ -39,52 +39,57 @@ let fighterPoints = 10;
 
 
 let fight = function (oppName) {
-    window.alert("Welcome to Robo-Glads! The Ultimate Fighter Game!")
     // below will contain prompt to let fight begin or stop
     let fightOrNot = window.prompt("Would you like to fight or are you chicken? Enter 'Fight' or 'Chicken' to let the game begin!")
 
-    if (fightOrNot === "fight" || fightOrNot === "FIGHT" || fightOrNot === "Fight") {
+    while(oppHealth > 0) {
+        if (fightOrNot === "fight" || fightOrNot === "FIGHT" || fightOrNot === "Fight") {
 
-        //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-        oppHealth = oppHealth - fighterAttack;
-
-        // Log a resulting message to the console so we know that it worked.
-        console.log(fighterName + 'Attacked' + oppName + ". " + oppName + ' now has ' + oppHealth + ' health remaining.');
-
-        // this will check the opps health 
-        if (oppHealth <= 0) {
-            window.alert(oppName + ' has fallen to the underworld')
-        } else {
-            window.alert(oppName + ' still has' + oppHealth + ' health left.');
+            //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+            oppHealth = oppHealth - fighterAttack;
+    
+            // Log a resulting message to the console so we know that it worked.
+            console.log(fighterName + 'Attacked' + oppName + ". " + oppName + ' now has ' + oppHealth + ' health remaining.');
+    
+            // this will check the opps health 
+            if (oppHealth <= 0) {
+                window.alert(oppName + ' has fallen to the underworld')
+            } else {
+                window.alert(oppName + ' still has' + oppHealth + ' health left.');
+            }
+    
+    
+            // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+            fighterHealth = fighterHealth - oppAttack;
+    
+            // Log a resulting message to the console so we know that it worked.
+            console.log(oppName + "attacked " + fighterName + '. ' + fighterName + " now has " + fighterHealth + " health remaining.")
+    
+            if (fighterHealth <= 0) {
+                window.alert(figtherName + ' has died.')
+            } else {
+                window.alert(fighterName + ' still has ' + fighterHealth + ' health left.')
+            }
+            // if fighter chickens out
+    
+        } else if (fightOrNot === 'chicken' || fightOrNot === 'CHICKEN' || fightOrNot === "Chicken") {
+            let fighterIsChicken = window.confirm('Are you sure you are a chicken?');
+            if (fighterIsChicken) {
+                window.alert(fighterName + " has decided to enter the chicken coop. Bacoooooooccccckkkkkk!!!!!");
+                fighterPoints = fighterPoints - 2;
+            } else {
+                fight(enemyRobot)
+            }
         }
+    
 
-
-        // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-        fighterHealth = fighterHealth - oppAttack;
-
-        // Log a resulting message to the console so we know that it worked.
-        console.log(oppName + "attacked " + fighterName + '. ' + fighterName + " now has " + fighterHealth + " health remaining.")
-
-        if (fighterHealth <= 0) {
-            window.alert(figtherName + ' has died.')
-        } else {
-            window.alert(fighterName + ' still has ' + fighterHealth + ' health left.')
-        }
-        // if fighter chickens out
-
-    } else if (fightOrNot === 'chicken' || fightOrNot === 'CHICKEN' || fightOrNot === "Chicken") {
-        let fighterIsChicken = window.confirm('Are you sure you are a chicken?');
-        if (fighterIsChicken) {
-            window.alert(fighterName + " has decided to enter the chicken coop. Bacoooooooccccckkkkkk!!!!!");
-            fighterPoints = fighterPoints - 2;
-        } else {
-            fight(enemyRobot)
-        }
     }
+
 
 }
 
 for (let i = 0; i < oppNames.length; i++) {
+    debugger;
     fight(oppNames[i])
 }
 
