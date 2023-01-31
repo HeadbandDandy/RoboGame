969e1aa0b80228fa764162490f60892a1739c2ec
Darryl Johnson <dandycodes@gmail.com>
2023-01-30 21:42:20 -0500

    added Opps

--- game.js
+++ game.js
@@ -2,6 +2,14 @@
 // game start, game end, fighting points(randomized), enemy abilities, and power ups
 
 
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+
+
 let fighterName = window.prompt("What is the name of your fighter?")
 
 let fighterHealth = 100;
@@ -18,17 +26,19 @@ console.log(fighterAttack, fighterHealth, fighterName);
 
 
 // below contains variables for enemy
-let oppName = "Oppenheimer";
+let oppNames = ['Oppenheimer', 'Optimus Prime', '300Block-Opp'];
 
 let oppHealth = 50;
 
 let oppAttack = 12;
 
+
+
 // below will contain the players abilities/currency
 let fighterPoints = 10;
 
 
-let fight = function () {
+let fight = function (oppName) {
     window.alert("Welcome to Robo-Glads! The Ultimate Fighter Game!")
     // below will contain prompt to let fight begin or stop
     let fightOrNot = window.prompt("Would you like to fight or are you chicken? Enter 'Fight' or 'Chicken' to let the game begin!")
@@ -39,13 +49,13 @@ let fight = function () {
         oppHealth = oppHealth - fighterAttack;
 
         // Log a resulting message to the console so we know that it worked.
-        console.log(fighterName + 'Attacked' + oppName + ". " + oppName + 'now has ' + oppHealth + ' health remaining.');
+        console.log(fighterName + 'Attacked' + oppName + ". " + oppName + ' now has ' + oppHealth + ' health remaining.');
 
         // this will check the opps health 
         if (oppHealth <= 0) {
             window.alert(oppName + ' has fallen to the underworld')
         } else {
-            window.alert(oppName + ' still has' + oppHealth + 'health left.');
+            window.alert(oppName + ' still has' + oppHealth + ' health left.');
         }
 
 
@@ -53,7 +63,7 @@ let fight = function () {
         fighterHealth = fighterHealth - oppAttack;
 
         // Log a resulting message to the console so we know that it worked.
-        console.log(oppName + "attacked " + fighterName + '. ' + fighterName + "now has " + fighterHealth + " health remaining.")
+        console.log(oppName + "attacked " + fighterName + '. ' + fighterName + " now has " + fighterHealth + " health remaining.")
 
         if (fighterHealth <= 0) {
             window.alert(figtherName + ' has died.')
@@ -68,13 +78,16 @@ let fight = function () {
             window.alert(fighterName + " has decided to enter the chicken coop. Bacoooooooccccckkkkkk!!!!!");
             fighterPoints = fighterPoints - 2;
         } else {
-            fight()
+            fight(enemyRobot)
         }
     }
 
 }
 
+for (let i = 0; i < oppNames.length; i++) {
+    fight(oppNames[i])
+}
 
 
-//callback/execution function
-fight(); 
+
+
