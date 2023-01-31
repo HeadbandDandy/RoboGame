11cebc897211cd48a393f49e54e10d5885209807
Darryl Johnson <dandycodes@gmail.com>
2023-01-30 20:33:53 -0500

    initial game set up working

--- game.js
+++ game.js
@@ -3,19 +3,62 @@
 
 
 let fighterName = window.prompt("What is the name of your fighter?")
-window.alert(fighterName)
-console.log(fighterName)
-console.log("This log is for strings, great for leaving messages")
-console.log(10 + 10)
-console.log("Our fighters Name is " + fighterName)
+
+let fighterHealth = 100;
+
+// below will check to see if the fighter has died
+if(fighterHealth > 0) {
+    console.log('Your Player is still alive');
+}
+
+let fighterAttack = 10;
+
+//log for above variables
+console.log(fighterAttack, fighterHealth, fighterName);
 
 
+// below contains variables for enemy
+let oppName = "Oppenheimer";
 
-// function below will start the game/battle
-function fight() {
-    window.alert('Let The Game Begin!!!')
+let oppHealth = 50;
+
+let oppAttack = 12;
+
+
+let fight = function () {
+    window.alert("Welcome to Robo-Glads! The Ultimate Fighter Game!")
+
+
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    oppHealth = oppHealth - fighterAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(fighterName + 'Attacked' + oppName + ". " + oppName + 'now has ' + oppHealth + ' health remaining.');
+
+    // this will check the opps health 
+    if (oppHealth <= 0) {
+        window.alert(oppName + ' has fallen to the underworld')
+    } else {
+        window.alert(oppName + ' still has' + oppHealth + 'health left.');
+    }
+
+
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    fighterHealth = fighterHealth - oppAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(oppName + "attacked " + fighterName + '. ' + fighterName + "now has " + fighterHealth + " health remaining.")
+
+    if(fighterHealth <= 0) {
+        window.alert(figtherName + ' has died.')
+    } else {
+        window.alert(fighterName + ' still has ' + fighterHealth + ' health left.')
+    }
+
+
 }
 
 
-//callback function
-// fight(); dont need this line to run yet
+
+//callback/execution function
+fight(); 
